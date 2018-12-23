ed41fbc69981255a303f61b830efffd82d1473f0
largezhou <largezhou@163.com>
2018-12-23 19:17:52 +0800

    去掉mix-manifest.json文件

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,8 @@
 const mix = require('laravel-mix')
 const webpack = require('webpack')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+// 去掉生成 mix-manifest.json 文件
+Mix.manifest.refresh = () => {}
 
 mix
     .options({
