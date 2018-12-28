12536850ab9ce861440f273c865b6d3ff99ce1b7
largezhou <largezhou@163.com>
2018-12-28 15:09:01 +0800

    改个方法名

--- src/WechatMenu.php
+++ src/WechatMenu.php
@@ -5,12 +5,24 @@ namespace Largezhou\WechatMenu;
 class WechatMenu
 {
     /**
+     * 返回页面内容前，加上请求地址前缀的元素
+     *
+     * @param $html
+     *
+     * @return string
+     */
+    protected static function response($html)
+    {
+        return Config::renderPrefixEl().$html;
+    }
+
+    /**
      * 返回编辑菜单的 html 页面内容
      *
      * @return string
      */
-    public static function renderMenus()
+    public static function renderMenusEditor()
     {
-        return Config::renderPrefixEl().file_get_contents(__DIR__.'/../resources/views/menus.html');
+        return static::response(file_get_contents(__DIR__.'/../resources/views/menus.html'));
     }
 }
