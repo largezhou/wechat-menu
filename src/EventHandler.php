fbe42c0db305d38ae67f2fe98379dafcc49f8b01
largezhou <largezhou@163.com>
2019-01-03 18:38:57 +0800

    处理方法适应其他事件的处理

--- src/EventHandler.php
+++ src/EventHandler.php
@@ -17,6 +17,13 @@ class EventHandler implements EventHandlerInterface
         'location_select', // 位置选择
     ];
 
+    // 订阅和取消订阅的 MsgType 为 event，单独处理一下
+    // 其他则直接用 MsgType 判断
+    const SUBSCRIBE_EVENTS = [
+        'subscribe',
+        'unsubscribe',
+    ];
+
     protected $logger;
     protected $manager;
 
@@ -26,7 +33,7 @@ class EventHandler implements EventHandlerInterface
         $this->logger = $this->manager->getWechat()->logger;
     }
 
-    public function handle($payload = null)
+    public function handle($payload = null): string
     {
         $this->logger->info("[payload]\n".json_encode($payload, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT));
 
@@ -36,6 +43,8 @@ class EventHandler implements EventHandlerInterface
 
         if ($msgType == 'event' && in_array($eventType, static::MENU_EVENTS)) {
             return $this->handleMenuEvent($payload, $eventKey);
+        } else {
+            return $this->handleOtherEvent($payload, $msgType, $eventType);
         }
     }
 
@@ -47,26 +56,59 @@ class EventHandler implements EventHandlerInterface
      *
      * @return string
      */
-    protected function handleMenuEvent($payload, $eventKey)
+    protected function handleMenuEvent(array $payload, string $eventKey): string
     {
         if (!$eventKey) {
             $this->logger->error('无法获取到 EventKey');
 
-            return;
+            return '';
         }
 
+        return $this->resolveHandler($payload, 'menu_events', $eventKey);
+    }
+
+    /**
+     * 处理其他事件
+     *
+     * @param array  $payload
+     * @param string $msgType
+     * @param string $eventType
+     *
+     * @return string
+     */
+    protected function handleOtherEvent(array $payload, string $msgType, string $eventType = null): string
+    {
+        // 订阅和取消订阅的 MsgType 为 event，单独处理一下
+        if ($msgType == 'event' && in_array($eventType, static::SUBSCRIBE_EVENTS)) {
+            $msgType = $eventType;
+        }
+
+        return $this->resolveHandler($payload, 'other_events', $msgType);
+    }
+
+    /**
+     * 调用回调
+     *
+     * @param array  $payload
+     * @param string $dataType 回调设置数据中的 key（分组）
+     * @param string $key 事件标识
+     *
+     * @return string
+     */
+    protected function resolveHandler(array $payload, string $dataType, string $key): string
+    {
         $eventHandler = null;
-        foreach (Data::getData('menu_events') as $e) {
-            if ($e['key'] == $eventKey) {
+        foreach (Data::getData($dataType) as $e) {
+            if ($e['key'] == $key) {
                 $eventHandler = $e;
                 break;
             }
         }
 
         if (!$eventHandler) {
-            $this->logger->error("没有与 {$eventKey} 对应的处理方法");
+            $this->logger->error("没有与 {$key} 对应的处理方法");
 
-            return;
+            return '';
         }
 
         $type = $eventHandler['type'];
